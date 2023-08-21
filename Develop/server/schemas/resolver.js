d32de6deb139ceb1dc1387b1cfef4069724ddd9a
AntimatterNova <47792025+AntimatterNova@users.noreply.github.com>
2023-08-21 17:30:26 -0500

    Finished index.js and resolver.js in the schema folder

---
+++ Develop/server/schemas/resolver.js
@@ -0,0 +1,75 @@
+const { User } = require('../models');
+const Book = require('../models/Book');
+const { signToken, AuthError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find();
+    },
+    user: async (parent, {userId}) => {
+        console.log(`Requested User ID => `+userId)
+      return User.findOne({ _id: userId })
+      .select('-__v')
+      .populate('savedBooks');
+    },
+    me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        }
+        throw AuthError;
+      },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthError;
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthError;
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, {authors,bookId,title,link,image,description}, context) => {
+        if (bookId) {
+       return User.findOneAndUpdate(
+          { _id: context.user._id },
+          {$addToSet: {
+            savedBooks: {authors: authors,bookId,title,link,image,description}
+            }},
+          {new: true}
+        ); 
+      }
+      throw AuthError;
+      ("You aren't logged in!");
+    },
+    removeBook: async (parent, { bookId }, context) => {
+        if (bookId) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { 
+            $pull: { 
+                savedBooks: {
+                    bookId: bookId 
+            } } },
+            {new: true}
+        );
+      }
+      throw AuthError;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ Develop/server/schemas/typedefs.js
@@ -0,0 +1,36 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: Int
+        savedBooks: [Book]!  
+    }
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String!
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        users: [User]
+        user(userId: ID!): User
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String]!, bookId: String!, title: String!, link: String, image: String, description: String): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typedefs;
