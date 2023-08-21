0646011ac040051869f663d27e60b82e758957d3
AntimatterNova <47792025+AntimatterNova@users.noreply.github.com>
2023-08-21 17:43:22 -0500

    Finished mutations.js and queries.js in the utils folder

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,66 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+//edit
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: [String]!, $bookId: String!, $title: String!, $link: String, $image: String, $description: String) {
+        saveBook(authors: $authors, bookId: $bookId, title: $title, link: $link, image: $image, description: $description) {
+            username
+            email
+            _id
+            bookCount
+            savedBooks {
+                _id
+                authors
+                bookId
+                description
+                image
+                link
+                title
+            }
+        }
+    }
+`;
+
+//edit
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            bookCount
+            email
+            username
+            savedBooks {
+                _id
+                authors
+                bookId
+                description
+                link
+                title
+                image
+            }
+        }
+    }
+`;
